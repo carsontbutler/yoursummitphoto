e44fa2fb91185bfd2a46d83afa608291871045f4
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2023-01-25 17:27:29 -0600

    url fixes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import UploadPhotoView, GetLocationsView, GetImageDataView, RegisterUserView, MyTokenObtainPairView
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
@@ -16,5 +18,7 @@ urlpatterns = [
     path('images/', GetImageDataView.as_view()),
     path('upload/', UploadPhotoView.as_view()),
     path('locations/', GetLocationsView.as_view()),
-
-]
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- frontend/src/components/store/api.js
+++ frontend/src/components/store/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = "yoursummitphoto-api.carsonbutler.dev";
+export const url = "https://yoursummitphoto-api.carsonbutler.dev";
 
 export const axiosInstance = axios.create({
   baseURL: url,
