6696f7fc56d44d79e7e31a7e8d2282d10416f98d
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2023-01-28 13:05:18 -0600

    register page functionality

--- frontend/src/components/pages/AuthPage/RegisterPage.js
+++ frontend/src/components/pages/AuthPage/RegisterPage.js
@@ -1,8 +1,54 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { axiosInstance, url } from "../../store/api";
 
 const RegisterPage = () => {
   const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const navigate = useNavigate();
+
+  const registerHandler = (e) => {
+    e.preventDefault();
+    setErrorMessage("");
+    setSuccessMessage("");
+    if (password !== password2) {
+      setErrorMessage("Passwords do not match.");
+      return;
+    }
+    else if (username.length > 150){
+      setErrorMessage("Username cannot be longer than 150 characters");
+      return;
+    };
+    axiosInstance
+      .post(`${url}/api/register/`, {
+        username: username,
+        password: password,
+        password2: password2,
+      })
+      .then((res) => {
+        console.log(res.data);
+        if (res.status === 201) {
+          setSuccessMessage("Success! Redirecting to login...");
+          setTimeout(() => {
+            navigate("/login");
+          }, 2000);
+        }
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+        if (
+          error.response.data.username
+        ) {
+          setErrorMessage("Username is already taken. Please try another.");
+        } else {
+          setErrorMessage("Something went wrong. Please try again later.");
+        }
+      });
+  };
+
   return (
     <div className="bg-mountain bg-cover bg-center flex h-screen max-w-full h-screen px-4">
       <div className="h-full sm:h-5/6 w-full md:w-4/6 lg:w-3/6 bg-blue2 bg-opacity-30 m-auto py-6 flex shadow-xl rounded animate-fadeIn">
@@ -11,7 +57,10 @@ const RegisterPage = () => {
             Create an account
           </h3>
           <div className="flex">
-            <form className="lg:w-5/6 xl:w-4/6 2xl:w-4/6 rounded px-8 pt-6 pb-8 mb-4 m-auto">
+            <form
+              onSubmit={registerHandler}
+              className="lg:w-5/6 xl:w-4/6 2xl:w-4/6 rounded px-8 pt-6 pb-8 mb-4 m-auto"
+            >
               <div className="mb-4">
                 <label
                   className="block text-white text-lg mb-2 drop-shadow-lg"
@@ -23,6 +72,11 @@ const RegisterPage = () => {
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="username"
                   type="text"
+                  name="username"
+                  value={username}
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                  }}
                 />
               </div>
               <div className="mb-4">
@@ -36,6 +90,11 @@ const RegisterPage = () => {
                   className="shadow appearance-none border border-blue2 rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
                   id="password"
                   type="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
                 />
               </div>
               <div className="mb-4">
@@ -49,19 +108,26 @@ const RegisterPage = () => {
                   className="shadow appearance-none border border-blue2 rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
                   id="password2"
                   type="password"
+                  name="password2"
+                  value={password2}
+                  onChange={(e) => {
+                    setPassword2(e.target.value);
+                  }}
                 />
               </div>
               <div className="text-center">
                 <button
                   className="bg-orange5 shadow-lg bg-opacity-70 hover:bg-orange1 text-white py-2 px-4 rounded transition ease-in-out delay-75"
-                  type="button"
+                  type="submit"
                 >
                   Register
                 </button>
+                <p className="text-orange5 mt-4 w-5/6 text-center m-auto">{successMessage}</p>
+                <p className="text-red mt-4 w-5/6 text-center m-auto">{errorMessage}</p>
                 <p className="text-white hover:text-orange2 text-md mt-4">
                   <Link to="/login">Already have an account? Sign in.</Link>
                 </p>
-                <p className="text-red mt-4">{errorMessage}</p>
+
                 <Link
                   to="/gallery"
                   className="text-white hover:text-orange2 text-md text-center underline"
